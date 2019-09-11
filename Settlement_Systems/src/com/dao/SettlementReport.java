e483660eb4c988665b03747f44f745addf8a8f2a
sulekhakri <48277338+sulekhakri@users.noreply.github.com>
2019-09-12 00:04:11 +0530

    Add files via upload
    FINAL SETTLEMENT UPDATED IN MASTER

--- Settlement_Systems/src/com/dao/SettlementReport.java
+++ Settlement_Systems/src/com/dao/SettlementReport.java
@@ -7,43 +7,77 @@ import com.pojos.Trader;
 
 public class SettlementReport {
 
-	public static void generateSettlement(Trader trader)
-	{
-		String traderId= trader.getTraderId();
-		List<Equity> equities=  ObjectsCreation.findEquityDetailsOfTrader(traderId);
-		 for(Equity equity:equities)
-		 {
-			 int correspShare = (CLASSNAME).getDataFromReportTable(trader.getTraderName(),equity.getEquityName());
-			 float fundBal= trader.getTraderFundBal();
-			 int equityBal= equity.getQuantity();
-			 if(correspShare >0)
-			 {
-				 equity.setQuantity(correspShare);
-			 }
-			 else {
-				 if(equityBal >= Math.abs(correspShare))
-				 {
-					 equity.setQuantity(equityBal-Math.abs(correspShare));
-				 }
-				 else {
-					 int shareShortgae= Math.abs(correspShare)-equityBal;
-					 float rate= (CLASSNAME).getBorrowRate(equity.getTickerSymbol());
-					 float shareCost= Math.multiplyExact(shareShortgae, rate);
-					 if(fundBal >= shareCost )
-					 {
-						 trader.setTraderFundBal(fundBal-shareCost);
-					 }
-					 else {
-						 float dueCost= shareCost-fundBal;
-						 trader.setTraderFundBal(0);
-						 //fund balance se bhi jyada hone pr apan ko bass show krna hai ki itta ka shortage rh gya hai
-						 //idk how to do that.
-					 }
-				 }
-			 }
-			 
-		 }
-		
-		
+	public void generateSettlement(Trader trader) {
+		AdminDAOImpl Dao = new AdminDAOImpl();
+		String traderId = trader.getTraderId();
+		List<Equity> equities = ObjectsCreation.findEquityDetailsOfTrader(traderId);
+		float grossFund = 0;
+		float fundBal = trader.getTraderFundBal();
+		for (Equity equity : equities) {
+			float correspFund = Dao.getDataFromFundReportTable(trader, equity);
+			grossFund += correspFund;
+
+		}
+		System.out.println("*****traderwise fundreport ***** " + grossFund + "   " + trader.getTraderId());
+		float netFund = grossFund + fundBal;
+
+		if (netFund < 0) {
+			float fundRate = Dao.getBorrowFundRate("usd");
+			double fundInterest = (fundRate * (2.0 / 365.0)) / 100;
+			double fundCost = netFund * fundInterest;
+			Dao.setDataInFundInterestTable(trader, fundCost);
+			netFund += fundCost;
+			trader.setTraderFundBal(netFund);
+			Dao.setTraderFund(trader);
+		} else {
+			Dao.setDataInFundInterestTable(trader, 0);
+			trader.setTraderFundBal(netFund);
+			Dao.setTraderFund(trader);
+		}
+	
+
+		for (Equity equity : equities) {
+			fundBal = trader.getTraderFundBal();
+			System.out.println(" FUND BALANCEEEEEEEEEEE    " + fundBal + "  and trader .."+ trader.getTraderId());
+			int correspShare = Dao.getDataFromReportTable(trader, equity);
+			int equityBal = equity.getQuantity();
+			if (correspShare >= 0) {
+				equityBal += correspShare;
+				equity.setQuantity(equityBal);
+				Dao.setTraderEquity(trader, equity);
+				Dao.setDataInEquityInterestTable(trader, equity, 0);
+				Dao.setDataInEquityShortageTable(trader, equity, 0);
+			} else {
+				if (equityBal >= Math.abs(correspShare)) {
+					equityBal += correspShare;
+					equity.setQuantity(equityBal);
+					Dao.setTraderEquity(trader, equity);
+					Dao.setDataInEquityInterestTable(trader, equity, 0);
+					Dao.setDataInEquityShortageTable(trader, equity, 0);
+				} else {
+					int shareShortage = equityBal - Math.abs(correspShare);
+					Dao.setDataInEquityShortageTable(trader, equity, shareShortage);
+					equityBal = 0;
+					equity.setQuantity(equityBal);
+					Dao.setTraderEquity(trader, equity);
+                     
+					float equityRate = Dao.getBorrowEquityRate(equity);
+					float equityPrice = Dao.getBorrowEquityPrice(equity);
+					double equityInterest = (equityPrice * equityRate * (2.0 / 365.0)) / 100;
+					double shareCost = shareShortage * equityInterest;
+					System.out.println("equity interest charge  ###### " + trader.getTraderId() + " "
+							+ equity.getEquityName() + " " + equityInterest + " INTERSEST $$$$$ " + shareCost);
+					Dao.setDataInEquityInterestTable(trader, equity, shareCost);
+					float remainingFunds = (float) (fundBal + shareCost);
+					System.out.println("after shortage FUND BALANCEEEEEEEEEEE    " + remainingFunds + "  and trader .."+ trader.getTraderId());
+					trader.setTraderFundBal(remainingFunds);
+					Dao.setTraderFund(trader);
+				}
+
+			}
+
+		}
+
 	}
+
 }
