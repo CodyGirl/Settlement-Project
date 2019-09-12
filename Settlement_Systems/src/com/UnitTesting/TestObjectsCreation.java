a50e1021f5ab53ca5311a382c80b1512730b2f46
Shekhar1996 <shekhar.mayank07@gmail.com>
2019-09-12 22:50:44 +0530

    Updated TestObjectsCreation

--- Settlement_Systems/src/com/UnitTesting/TestObjectsCreation.java
+++ Settlement_Systems/src/com/UnitTesting/TestObjectsCreation.java
@@ -2,6 +2,7 @@ package com.UnitTesting;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.After;
@@ -10,6 +11,8 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.dao.AdminDAO;
+import com.dao.AdminDAOImpl;
 import com.dao.ObjectsCreation;
 import com.pojos.Equity;
 import com.pojos.Trader;
@@ -32,44 +35,71 @@ public class TestObjectsCreation {
 	public void tearDown() throws Exception {
 	}
 
-	@Test
-	public void testFindEquityDetailsOfTrader() {
-		ObjectsCreation ob=new ObjectsCreation();
-		List<Equity> equity=ob.findEquityDetailsOfTrader("CITI");
-		Equity e=equity.get(3);
-		
-		assertEquals(5, equity.size());
-		assertEquals(454332, e.getQuantity());
-	}
+//	@Test
+//	public void testFindEquityDetailsOfTrader() {
+//		ObjectsCreation ob=new ObjectsCreation();
+//		List<Equity> equity=ob.findEquityDetailsOfTrader("CITI");
+//		Equity e=equity.get(3);
+//		
+//		assertEquals(5, equity.size());
+//		assertEquals(454332, e.getQuantity());
+//	}
 
-	@Test
-	public void testFindTrader() {
-		ObjectsCreation ob=new ObjectsCreation();
-		List<Trader> traders=ob.findTrader();
-		Trader trader=traders.get(4);
-		
-		assertEquals(5, traders.size());
-		assertEquals("CITI", trader.getTraderId());
-	}
+//	@Test
+//	public void testFindTrader() {
+//		ObjectsCreation ob=new ObjectsCreation();
+//		List<Trader> traders=ob.findTrader();
+//		Trader trader=traders.get(4);
+//		
+//		assertEquals(5, traders.size());
+//		assertEquals("CITI", trader.getTraderId());
+//	}
 
-	@Test
-	public void testFindTraderById() {
-		
-		ObjectsCreation ob=new ObjectsCreation();
-		Trader t =ob.findTraderById("JPM");
-		assertEquals(5322113.0f, t.getTraderFundBal(),0.001);
-		
-	}
+//	@Test
+//	public void testFindTraderById() {
+//		ObjectsCreation ob=new ObjectsCreation();
+//		Trader t =ob.findTraderById("JPM");
+//		assertEquals(5322113.0f, t.getTraderFundBal(),0.001);
+//	}
 
-	@Test
-	public void testFindEquity() {
-		ObjectsCreation ob=new ObjectsCreation();
-		List<Equity> equities=ob.findEquity();
-		
-		Equity e=equities.get(1);
-		
-		assertEquals(0.05f, e.getFaceValue(),0.001);
-		assertEquals(5, equities.size());
-	}
+//	@Test
+//	public void testFindEquity() {
+//		ObjectsCreation ob=new ObjectsCreation();
+//		List<Equity> equities=ob.findEquity();
+//		
+//		Equity e=equities.get(1);
+//		
+//		assertEquals(0.05f, e.getFaceValue(),0.001);
+//		assertEquals(5, equities.size());
+//	}
+
+//	@Test
+//	public void testFindTradersBySecurity() {
+//		String tickerSymbol="FB";
+//		
+//		List<Trader> list=new ArrayList<>();
+//		ObjectsCreation ob=new ObjectsCreation();
+//		list=ob.findTradersBySecurity(tickerSymbol);
+//		
+//		Trader trader=list.get(1);
+//		
+//		assertEquals(5,list.size());
+//		assertEquals("DB", trader.getTraderId());
+//		
+//	}
+
+//	@Test
+//	public void testGetNumberOfEquityByName() {
+//		ObjectsCreation ob=new ObjectsCreation();
+//		Trader trader=new Trader();
+//		trader.setTraderId("BCS");
+//		String tickersymbol="MSFT";
+//		assertEquals(33, ob.getNumberOfEquityByName(tickersymbol, trader));
+//	}
+
+//	@Test
+//	public void testUpdateFaceValue() {
+//		fail("Not yet implemented");
+//	}
 
 }
